c0a636a31b186e5c03d1f0c82f0ce67ad08c1329
CostaFelipe452 <felipe.452@hotmail.com>
2025-02-05 10:24:15 -0300

    change name

---
+++ deploy.R
@@ -0,0 +1,11 @@
+library(rsconnect)
+
+readRenviron("config/.Renviron")
+
+shiny_name <- Sys.getenv("shiny_name")
+shiny_token <- Sys.getenv("shiny_token")
+shiny_secret <- Sys.getenv("shiny_secret")
+
+rsconnect::setAccountInfo(name = shiny_name, token = shiny_token, secret = shiny_secret)
+
+rsconnect::deployApp(appDir = getwd())
